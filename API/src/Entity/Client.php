aea5ff78d601dd1dcab0912d06a6f5763dcf816e
robinivan <72008804+robinivan@users.noreply.github.com>
2021-06-15 21:09:07 +0300

    redy

---
+++ API/src/Entity/Client.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClientRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ClientRepository::class)
+ */
+class Client
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $first_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $last_name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $transaction_number;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $access;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $pet_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName(string $first_name): self
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->last_name;
+    }
+
+    public function setLastName(string $last_name): self
+    {
+        $this->last_name = $last_name;
+
+        return $this;
+    }
+
+    public function getTransactionNumber(): ?int
+    {
+        return $this->transaction_number;
+    }
+
+    public function setTransactionNumber(int $transaction_number): self
+    {
+        $this->transaction_number = $transaction_number;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getAccess(): ?int
+    {
+        return $this->access;
+    }
+
+    public function setAccess(int $access): self
+    {
+        $this->access = $access;
+
+        return $this;
+    }
+
+    public function getPetId(): ?int
+    {
+        return $this->pet_id;
+    }
+
+    public function setPetId(?int $pet_id): self
+    {
+        $this->pet_id = $pet_id;
+
+        return $this;
+    }
+}
---
+++ API/src/Entity/Command.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommandRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommandRepository::class)
+ */
+class Command
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $users_id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $product_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $reference;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $storage;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $deliver_day;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsersId(): ?int
+    {
+        return $this->users_id;
+    }
+
+    public function setUsersId(int $users_id): self
+    {
+        $this->users_id = $users_id;
+
+        return $this;
+    }
+
+    public function getProductName(): ?string
+    {
+        return $this->product_name;
+    }
+
+    public function setProductName(string $product_name): self
+    {
+        $this->product_name = $product_name;
+
+        return $this;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setReference(string $reference): self
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    public function getStorage(): ?string
+    {
+        return $this->storage;
+    }
+
+    public function setStorage(string $storage): self
+    {
+        $this->storage = $storage;
+
+        return $this;
+    }
+
+    public function getDeliverDay(): ?\DateTimeInterface
+    {
+        return $this->deliver_day;
+    }
+
+    public function setDeliverDay(\DateTimeInterface $deliver_day): self
+    {
+        $this->deliver_day = $deliver_day;
+
+        return $this;
+    }
+}
